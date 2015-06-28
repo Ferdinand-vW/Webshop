3a81c71fba819de4a3b607865475932f5fb924c7
Ferdinand-vW <ferdinandvwalree@gmail.com>
2015-06-29 01:02:13 +0200

    Added admincontrolpanel controller

---
+++ Webshop/Controllers/AdminControlPanelController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webshop.Controllers
+{
+    public class AdminControlPanelController : Controller
+    {
+        //
+        // GET: /AdminControlPanel/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
